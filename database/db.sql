de4d05aa8336471b0a89c258cc9e9376f28ba7b1
iiAnderson <ra12g14@gmail.com>
2024-07-13 17:24:48 +0100

    Add database spin up

---
+++ database/db.sql
@@ -0,0 +1,17 @@
+create table service_updates (
+    update_id UUID PRIMARY KEY,
+    ts TIMESTAMP NOT NULL,
+    rid varchar(30) NOT NULL,
+    uid varchar(10) NOT NULL,
+    passenger BOOLEAN NOT NULL
+);
+create table locations (
+    update_id UUID PRIMARY KEY,
+    service_update_id UUID,
+    time TIME NOT NULL,
+    tpl varchar(10) NOT NULL,
+    type varchar(10) NOT NULL,
+    CONSTRAINT service_updates
+        FOREIGN KEY(service_update_id) 
+        REFERENCES service_updates(update_id)
+);

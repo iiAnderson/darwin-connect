766251c73f0349dad71ba91dc322a9f34ce54435
iiAnderson <ra12g14@gmail.com>
2024-07-18 14:27:55 +0100

    Added TS parser

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from datetime import datetime
 
+from src.ts import TSMessage
+
 from clients.src.stomp import MessageParserInterface
 from models.src.schedule import InvalidServiceUpdate, ScheduleMessage
 
@@ -36,5 +38,38 @@ class ScheduleParser(MessageParserInterface):
 
         for message in schedules:
             messages.append(ScheduleMessage.create(message, ts))
+
+        return messages
+
+class TSParser(MessageParserInterface):
+
+    def parse(self, raw_body: dict) -> list[ScheduleMessage]:
+
+        try:
+            data = raw_body["Pport"]
+        except KeyError:
+            print("No Pport found in schedule message")
+            return []
+
+        try:
+            ts = datetime.fromisoformat(data["@ts"])
+        except KeyError as exception:
+            raise InvalidServiceUpdate(f"Cannot extract ts from {data}") from exception
+
+        try:
+            ur = data["uR"]
+
+            ts = ur["TS"]
+
+            if type(ts) is dict:
+                ts = [ts]
+
+        except KeyError as exception:
+            raise InvalidServiceUpdate(f"Cannot extract uR or schedule from {data}") from exception
+
+        messages = []
+
+        for message in ts:
+            messages.append(TSMessage.create(message, ts))
 
         return messages

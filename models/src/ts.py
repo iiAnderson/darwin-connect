ad8743af4528418af7299b98204815c9be95f7be
Robbie Anderson <ra12g14@gmail.com>
2024-07-21 14:35:15 +0100

    Updated TS parsing

--- models/src/ts.py
+++ models/src/ts.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
+
 from dataclasses import dataclass
 from datetime import datetime
 
-from models.src.common import LocationType, LocationUpdate, ServiceUpdate, TimeType, WritableMessage
+from models.src.common import (
+    LocationType,
+    LocationUpdate,
+    ServiceUpdate,
+    TimeType,
+    WritableMessage,
+)
 
 
 class InvalidLocation(Exception): ...
@@ -31,7 +38,7 @@ class ServiceParser:
             uid = body["@uid"]
         except KeyError as exception:
             raise InvalidServiceUpdate(f"Cannot extract uid from {body}") from exception
-        
+
         return ServiceUpdate(rid, uid, ts)
 
 
@@ -52,55 +59,51 @@ class ArrivalParser:
 
     @classmethod
     def parse(cls, body: dict, tpl: str) -> list[LocationUpdate]:
-        
+
         try:
-            arr = body['ns5:arr']
+            arr = body["ns5:arr"]
         except KeyError:
             return []
-        
+
         updates = []
 
         for key, value in arr.items():
-                
+
             try:
                 time_type = TimeTypeParser.parse(key)
                 raw_ts = value if len(value.split(":")) == 3 else f"{value}:00"
 
-                updates.append(LocationUpdate(
-                    tpl, LocationType.ARR, time_type, datetime.strptime(raw_ts, "%H:%M:%S")
-                ))
+                updates.append(LocationUpdate(tpl, LocationType.ARR, time_type, datetime.strptime(raw_ts, "%H:%M:%S")))
 
             except InvalidTimeType:
-                continue   
-    
+                continue
+
         return updates
-    
+
 
 class DepartureParser:
 
     @classmethod
     def parse(cls, body: dict, tpl: str) -> list[LocationUpdate]:
-        
+
         try:
-            arr = body['ns5:dep']
+            arr = body["ns5:dep"]
         except KeyError:
             return []
 
         updates = []
 
         for key, value in arr.items():
-                
+
             try:
                 time_type = TimeTypeParser.parse(key)
                 raw_ts = value if len(value.split(":")) == 3 else f"{value}:00"
 
-                updates.append(LocationUpdate(
-                    tpl, LocationType.DEP, time_type, datetime.strptime(raw_ts, "%H:%M:%S")
-                ))
+                updates.append(LocationUpdate(tpl, LocationType.DEP, time_type, datetime.strptime(raw_ts, "%H:%M:%S")))
 
             except InvalidTimeType:
-                continue   
-        
+                continue
+
         return updates
 
 
@@ -111,13 +114,13 @@ class LocationsParser:
 
         locations = body["ns5:Location"]
 
-        if type(locations) == dict:
+        if type(locations) is dict:
             locations = [locations]
 
         updates: list[LocationUpdate] = []
 
         for locs in locations:
-    
+
             try:
                 tpl = locs["@tpl"]
             except KeyError:
@@ -134,6 +137,7 @@ class LocationsParser:
 
         return updates
 
+
 @dataclass
 class TSMessage(WritableMessage):
 
@@ -142,21 +146,15 @@ class TSMessage(WritableMessage):
 
     def get_locations(self) -> list[LocationUpdate]:
         return self.locations
-    
+
     def get_service(self) -> ServiceUpdate:
         return self.service
 
     @classmethod
     def create(cls, body: dict, ts: datetime) -> TSMessage:
-        
-        try:
-            ts_body = body['TS']
-        except KeyError:
-            raise InvalidTsMessage(f"Invalid Ts message {body}")
-        
-        locations = LocationsParser.parse(ts_body)
-        return cls(locations, ServiceParser.parse(ts_body, ts))
-        
+
+        locations = LocationsParser.parse(body)
+        return cls(locations, ServiceParser.parse(body, ts))
 
     def to_dict(self) -> dict:
         return {
@@ -169,7 +167,6 @@ class TSMessage(WritableMessage):
                     {"tpl": loc.tpl, "type": str(loc.type.value), "time": loc.timestamp.strftime("%H:%M:%S")}
                     for loc in self.locations
                 ],
-                key=lambda x: x['time']
-            )
+                key=lambda x: x["time"],
+            ),
         }
-    
